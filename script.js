522e49479863f6d3bbdf2bcbd75d82302a253ee5
android-013 <azrifat013@gmail.com>
2025-02-17 00:05:20 +0600

    shit

--- script.js
+++ script.js
@@ -6,9 +6,9 @@ document.addEventListener("DOMContentLoaded", () => {
     
     const checkWinner = () => {
         const winningCombinations = [
-            [0, 1, 2], [3, 4, 5], [6, 7, 8], 
-            [0, 3, 6], [1, 4, 7], [2, 5, 8],
-            [0, 4, 8], [2, 4, 6]
+            [1, 2, 3], [1, 4, 7], [1, 5, 9], 
+            [4, 5, 6], [2, 5, 8], [3, 5, 7],
+            [7, 8, 9], [3, 6, 9]
         ];
         
         for (let combination of winningCombinations) {
